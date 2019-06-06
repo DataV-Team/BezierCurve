9c35cce92b955eb14fc40f04df5848eaa3be9bc1
jiaming743 <743192023@qq.com>
2019-06-06 19:13:41 +0800

    update dir path

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import bezierCurveToPolyline from './core/bezierCurveToPolyline'
+
+import { getBezierCurveLength } from './core/bezierCurveToPolyline'
+
+import polylineToBezierCurve from './core/polylineToBezierCurve'
+
+export default {
+  bezierCurveToPolyline,
+  getBezierCurveLength,
+  polylineToBezierCurve
+}
